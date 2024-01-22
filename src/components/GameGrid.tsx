70c70777154043b6b02545da9a29b2ea4f1c56d8
Esmatullah Mohammadi <esmatullah1382@gmail.com>
2024-01-22 09:35:41 +0430

    create a custome hook to fetch the games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,32 +1,18 @@
-import { useEffect, useState } from "react";
 import { Text } from "@chakra-ui/react";
-import ApiClient from "../services/Api-client";
+import useGames from "../hook/useGames";
 
-interface Game {
-  id: number;
-  name: string;
-}
-interface FetchGameResponse {
-  count: number;
-  results: Game[];
-}
 
 const GameGrid = () => {
-  const [game, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    ApiClient.get<FetchGameResponse>("/games")
-      .then(res => setGames(res.data.results))
-      .catch(err => setError(err.message));
-  });
+  const {game,error}=useGames();
 
   return (
     <>
-        {error&&<Text>{error}</Text>}
-       <ul>
-            {game.map(g=> <li key={g.id}>{g.name}</li>)}
-       </ul>
+      {error && <Text>{error}</Text>}
+      <ul>
+        {game.map((g) => (
+          <li key={g.id}>{g.name}</li>
+        ))}
+      </ul>
     </>
   );
 };
---
+++ src/hook/useGames.ts
@@ -0,0 +1,32 @@
+import { useState,useEffect } from "react";
+import ApiClient from "../services/Api-client";
+import { CanceledError } from "axios";
+
+export interface Game {
+    id: number;
+    name: string;
+  }
+  interface FetchGameResponse {
+    count: number;
+    results: Game[];
+  }
+
+const useGames =()=>{
+    const [game, setGames] = useState<Game[]>([]);
+    const [error, setError] = useState("");
+  
+    useEffect(() => {
+      const controller= new AbortController();
+      ApiClient
+        .get<FetchGameResponse>("/games",{signal:controller.signal})
+        .then((res) => setGames(res.data.results))
+        .catch((err) =>{
+            if(err instanceof CanceledError) return;
+            setError(err.message)
+        });
+    },[]);
+
+    return {game,error};
+}
+
+export default useGames;
