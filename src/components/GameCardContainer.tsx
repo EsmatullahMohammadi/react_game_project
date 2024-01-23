a8820594023db98ce3d78ed34d02bcac0f6467c0
Esmatullah Mohammadi <esmatullah1382@gmail.com>
2024-01-23 11:03:18 +0430

    Display geners

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,7 @@ interface Probs {
 
 const GameCardContainer = ({ children }: Probs) => {
   return (
-    <Box width={"300px"} borderRadius={10} overflow={"hidden"}>
+    <Box borderRadius={10} overflow={"hidden"}>
         {children}
     </Box>
   );
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,27 @@
-import { Genre } from '../hook/useGenres';
-import useGenres from '../hook/useGenres'
-import useData from '../hook/useData'
+import { Genre } from "../hook/useGenres";
+import useGenres from "../hook/useGenres";
+import useData from "../hook/useData";
+import { HStack, List, ListItem, Image,Text } from "@chakra-ui/react";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-    const{data}=useGenres();
+  const { data } = useGenres();
   return (
-    <ul>
-        {data.map(dat=> <li key={dat.id}>{dat.name}</li>)}
-    </ul>
-  )
-}
+    <List>
+      {data.map((dat) => (
+        <ListItem key={dat.id} paddingY="5px">
+          <HStack>
+            <Image
+              src={getCroppedImageUrl(dat.image_background)}
+              boxSize={"32px"}
+              borderRadius={8}
+            ></Image>
+            <Text fontSize={'lg'}>{dat.name}</Text>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
 
-export default GenreList
+export default GenreList;
