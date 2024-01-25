8ef0a020e0ab112a67829c475d276f2bf0021d31
Esmatullah Mohammadi <esmatullah1382@gmail.com>
2024-01-25 12:13:43 +0430

    Clear the game card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,12 +13,11 @@ const GameCard = ({game}:Probs) => {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)}/>
       <CardBody>
-        <Heading fontStyle={"2xl"}>{game.name}</Heading>
-        <HStack justifyContent={'space-between'}>
+        <HStack marginBottom={3} justifyContent={'space-between'}>
           <PlatformIconList platforms={game.parent_platforms.map(p=>p.platform)}/>
           <CritcScore score={game.metacritic}/>
         </HStack>
-        
+        <Heading fontStyle={"2xl"}>{game.name}</Heading>
       </CardBody>
     </Card>
   )
