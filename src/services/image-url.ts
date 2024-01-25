297ae2f723bbce26f257fa36a37082bf00988d39
Esmatullah Mohammadi <esmatullah1382@gmail.com>
2024-01-25 10:37:45 +0430

    Handles games with no images

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,8 @@
+import noImage from '../assets/images.jpeg';
 
 const getCroppedImageUrl = (url: string) =>{
-    if(!url) return "";
+    if(!url) return noImage;
+
     const target= 'media/';
     const index=url.indexOf(target)+'media/'.length;
     return url.slice(0,index)+'crop/600/400/'+url.slice(index);
