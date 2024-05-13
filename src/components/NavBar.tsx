47c22d716f02ad363e528ccf30d73800266fe5a6
Esmatullah Mohammadi <esmatullah1382@gmail.com>
2024-05-13 22:33:51 +0430

    Fix the NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,13 +2,16 @@ import {  HStack,Image} from '@chakra-ui/react'
 import logo from '../assets/logo.jpg';
 import ColorModeSwitch from './ColorModeSwitch';
 import SearchInput from './SearchInput';
+import { Link } from 'react-router-dom';
 
 
 
 const NavBar = () => {
   return (
     <HStack  padding={'10px'}>
-        <Image src={logo} boxSize={'60px'} rounded={35}></Image>
+        <Link to={'/'}>
+          <Image src={logo} boxSize={'60px'} rounded={35} objectFit={'cover'}/>
+        </Link>       
         <SearchInput  />
         <ColorModeSwitch/>
     </HStack>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,17 +2,21 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef} from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../store";
-
+import { Navigate, useNavigate } from "react-router-dom";
 
 
 const SearchInput = () => {
     const ref= useRef<HTMLInputElement>(null);
     const setSearchText=useGameQueryStore(s=>s.setSearchText);
+    const navigate=useNavigate();
   return (
     <form onSubmit={(event)=>{
         //by preventDeafault we can access the value of this form
         event.preventDefault();
-        if(ref.current) setSearchText(ref.current.value);
+        if(ref.current){
+           setSearchText(ref.current.value);
+           navigate('/');
+          };
     }}>
         <InputGroup>
           <InputLeftElement children={<BsSearch />} />
